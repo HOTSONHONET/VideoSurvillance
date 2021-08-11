c5e67ec6657d21d6e30c2d3b2087319801056f41
HOTSONHONET <hotsonhonet@gmail.com>
2021-08-11 12:28:49 +0530

    removed model.py

--- backend/src/camera.py
+++ backend/src/camera.py
@@ -1,8 +1,15 @@
 import cv2
 import numpy as np
-from tensorflow.keras.models import load_model
+
 import imutils
 from config import *
+import os
+from glob import glob
+
+# Stopping TF warnings, info and print statements
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+import tensorflow
+from tensorflow.keras.models import load_model
 
 
 def mean_squared_loss(x1, x2):
@@ -25,6 +32,8 @@ class VideoCamera():
         self.cap = cv2.VideoCapture(path)
         self.cap.set(cv2.CAP_PROP_FPS, 5)
         self.losses = []
+        self.to_return = {}
+        self.count = 0
 
     def __del__(self):
         self.cap.release()
@@ -33,42 +42,69 @@ class VideoCamera():
         while True:
             imagedump = []
             ret, frame = self.cap.read()
-            if not ret:
+            self.count += 1
+            
+            if ret:
+                for _ in range(10):
+                    ret, frame = self.cap.read()
+                    image = imutils.resize(frame, width=1000, height=1200)
+
+                    frame = cv2.resize(frame, (227, 227),
+                                    interpolation=cv2.INTER_AREA)
+                    gray = 0.2989*frame[:, :, 0]+0.5870 * \
+                        frame[:, :, 1]+0.1140*frame[:, :, 2]
+                    gray = (gray-gray.mean())/gray.std()
+                    gray = np.clip(gray, 0, 1)
+                    imagedump.append(gray)
+
+                imagedump = np.array(imagedump)
+
+                imagedump.resize(227, 227, 10)
+                imagedump = np.expand_dims(imagedump, axis=0)
+                imagedump = np.expand_dims(imagedump, axis=4)
+
+                output = model.predict(imagedump)
+
+                loss = mean_squared_loss(imagedump, output)
+                print(f"loss : {loss}")
+
+                # if loss>0.00068:
+                if loss > 0.000538:
+                    print('Abnormal Event Detected')
+                    cv2.putText(image, "Abnormal Event", (220, 100),
+                                cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 4)
+
+
+                cv2.imwrite(f"{SAVE_IMG_PATH}/{self.count}.jpg", image)
+                self.losses.append(loss)
+              
+            else:
                 break
+    
+    def generateVideo(self):
+        print("[INFO] Generating Video...")
+        frame_arrays = []
+        images = sorted([glob(SAVE_IMG_PATH + "/" + "*.jpg")], key=lambda x: int(x.split(".")[0]))
+        for i in range(len(images)):
+            cur_img = SAVE_IMG_PATH + "/" + images[i]
+            """
+            Reading images
 
-            for _ in range(10):
-                ret, frame = self.cap.read()
-                image = imutils.resize(frame, width=1000, height=1200)
+            """
+            cur_img = cv2.imread(cur_img)
+            h, w, l = cur_img.shape
+            size = (w, h)
+            for k in range(2):
+                frame_arrays.append(cur_img)
+        
+        output = cv2.VideoWriter(SAVE_IMG_PATH + "/" + "done.mp4", cv2.VideoWriter_fourcc(*'mp4v'), 30, size)
+        for i in range(len(frame_arrays)):
+            output.write(frame_arrays[i])
+        
+        output.release()
+    
+    def giveLosses(self):
+        return self.losses
 
-                frame = cv2.resize(frame, (227, 227),
-                                   interpolation=cv2.INTER_AREA)
-                gray = 0.2989*frame[:, :, 0]+0.5870 * \
-                    frame[:, :, 1]+0.1140*frame[:, :, 2]
-                gray = (gray-gray.mean())/gray.std()
-                gray = np.clip(gray, 0, 1)
-                imagedump.append(gray)
-
-            imagedump = np.array(imagedump)
-
-            imagedump.resize(227, 227, 10)
-            imagedump = np.expand_dims(imagedump, axis=0)
-            imagedump = np.expand_dims(imagedump, axis=4)
-
-            output = model.predict(imagedump)
-
-            loss = mean_squared_loss(imagedump, output)
-            print(f"loss : {loss}")
-
-            # if loss>0.00068:
-            if loss > 0.000538:
-                print('Abnormal Event Detected')
-                cv2.putText(image, "Abnormal Event", (220, 100),
-                            cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 4)
-
-            image = cv2.resize(image, (600, 550))
-            _, jpeg = cv2.imencode('.jpg', image)
-            self.losses.append(loss)
-            yield (
-                b'--frame\r\n'
-                b'Content-Type: image/jpeg\r\n\r\n' + jpeg.tobytes() + b'\r\n\r\n'
-            )
+          
+                
--- backend/src/routes.py
+++ backend/src/routes.py
@@ -1,12 +1,15 @@
 from flask import Response, render_template, current_app as app, request, flash
 from config import *
 from src.camera import *
+from pprint import pprint
 
+# Gobal variable to store losses
+LOSSES = []
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
-@app.route('/video_feed')
-def video_feed():
-    return Response(VideoCamera("backend\schoolfight.mp4")(), mimetype='multipart/x-mixed-replace; boundary=frame')
+@app.route("/done")
+def done():
+    return render_template("done.html")
